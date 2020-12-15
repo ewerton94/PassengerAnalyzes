cb9532bdeab39213f8b1f7655f2fb285a5bc995e
Ewerton Oliveira <ewerton.amorim@live.com>
2020-12-15 20:00:17 -0300

    #4

---
+++ frontend/src/pages/Inicio.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="q-pa-lg">
+    <transition
+      appear
+      enter-active-class="animated fadeIn"
+      leave-active-class="animated fadeOut"
+    >
+      <q-option-group
+        v-model="linhasEscolhidas"
+        :options="linhas"
+        name="nome"
+        color="green"
+        type="checkbox"
+      >
+
+      </q-option-group>
+
+    </transition>
+    <q-btn label="Enviar" flat color="primary" @click="enviarLinhas"/>
+    <q-inner-loading :showing="visible">
+      <q-spinner-oval size="50px" color="primary" />
+    </q-inner-loading>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  data: function () {
+    return {
+      linhasEscolhidas: [],
+      visible: true
+    }
+  },
+  created () {
+    this.start()
+  },
+  computed: {
+    ...mapState('Core', ['linhas'])
+  },
+  methods: {
+    ...mapActions('Core', ['listarLinhas']),
+    async start () {
+      await this.listarLinhas()
+      this.visible = false
+    },
+    enviarLinhas () {
+      var linhas = this.linhasEscolhidas.join(',')
+      this.$router.push(`/dashboard/?linhas=${linhas}`)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
