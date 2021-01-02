a021a3c8131318ad2363256d0a49963da6a6447a
Ewerton Oliveira <ewerton.amorim@live.com>
2021-01-02 18:43:32 -0300

    Script para alimentar o banco de dados

--- backend/start_data.py
+++ backend/start_data.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 from core.models import *
+import os
+import pytz
 #from read_table import *
 #from refresh_cars import *
+legenda_contexto = '200'
 
-pre = '../Resultados/'
+pre = '../Resultados/'+legenda_contexto+'/'
 pos = ''
 
 
@@ -13,6 +16,7 @@ dic_empresa_cittamobi = {e.nome_cittamobi: e for e in Empresa.objects.all()}
 def create_lines(df):
     print(df.columns)
     df2 = df[['company_name', 'number_line', 'name_line']].drop_duplicates()
+    df2 = df2[df2.company_name == 'Empresa São Francisco']
     numeros = Linha.objects.all().values_list('numero', flat=True)
     df2 = df2[~df2.number_line.astype(int).isin(numeros)]
     if df2.empty:
@@ -24,19 +28,20 @@ def create_lines(df):
         df2['number_line'].values,
         df2['name_line'].values
     )
-    Linha.objects.bulk_create(linhas)
+    Linha.objects.bulk_create(list(linhas))
     print('Linhas Criadas')
 
 
 def create_pontos(df):
     trechos_dict = {t.nome: t for t in Trecho.objects.all()}
-    trechos = np.array([t for t in df.TRECHO.unique() if not t in list(trechos_dict.keys())])
+    trechos = list(np.array([t for t in df.TRECHO.unique() if not t in list(trechos_dict.keys())]))
     if trechos:
         trechos = np.vectorize(lambda x: Trecho(nome=x))(trechos)
         Trecho.objects.bulk_create(list(trechos))
         trechos_dict = {t.nome: t for t in Trecho.objects.all()}
     if not Ponto.objects.all().exists():
         print(df.LAT.astype(float).values)
+        df = df.drop_duplicates(subset=['company', 'stop_id'])
         pontos = np.vectorize(lambda lat, lon, trecho, endereco, codigo, bairro: Ponto(
             lat = lat,
             lon = lon,
@@ -55,34 +60,36 @@ def create_pontos(df):
             df.bairro.values,
         )
         Ponto.objects.bulk_create(list(pontos))
-    pontos = {p.codigo: p for p in Ponto.objects.all()}
-    ppe = np.vectorize(lambda p, e, c: PontoPorEmpresa(
-        ponto = pontos[p],
-        empresa=dic_empresa_cittamobi[e],
-        codigo_cittamobi=c
+    if not PontoPorEmpresa.objects.all().exists():
+        pontos = {p.codigo: p for p in Ponto.objects.all()}
+        ppe = np.vectorize(lambda p, e, c: PontoPorEmpresa(
+            ponto = pontos[p],
+            empresa=dic_empresa_cittamobi[e],
+            codigo_cittamobi=c
 
-    ))(
-        df.stop_id.values,
-        df.company.values,
-        df.stop_id.values,
-    )
-    PontoPorEmpresa.objects.bulk_create(list(ppe))
+        ))(
+            df.stop_id.values,
+            df.company.values,
+            df.stop_id.values,
+        )
+        PontoPorEmpresa.objects.bulk_create(list(ppe))
 
 
 
 def create_viagens(df):
-    dic_linhas = {l.numero: l for l in Linha.objects.all()}
+    dic_linhas = {int(l.numero): l for l in Linha.objects.all()}
     dic_carros = {c.numero: c for c in Carro.objects.all()}
 
     df1 = df[~df.linha.isna()]
     df2 = df1.drop_duplicates(['LINHA', 'CARRO', 'partida'])
+    maceio_tz = pytz.timezone("UTC")
 
     Partida.objects.all().delete()
     partidas = np.vectorize(lambda linha, atendimento, carro, horario, sentido: Partida(
-        linha=dic_linhas[str(int(linha))],
+        linha=dic_linhas[int(linha)],
         atendimento=atendimento,
         carro=dic_carros[str(carro)],
-        horario_prevista_terminal=pd.to_datetime(horario),
+        horario_prevista_terminal=maceio_tz.localize(pd.to_datetime(horario)),
         sentido=sentido
     ))(
         df2.LINHA.values,
@@ -94,25 +101,23 @@ def create_viagens(df):
     Partida.objects.bulk_create(list(partidas))
 
 
-    dic_partidas = {(p.linha.numero, p.carro.numero, pd.to_datetime(p.horario_prevista_terminal).date(), pd.to_datetime(p.horario_prevista_terminal).time()):p for p in Partida.objects.select_related().all()}
+    dic_partidas = {(int(p.linha.numero), p.carro.numero, pd.to_datetime(p.horario_prevista_terminal).strftime('%d/%m/%Y'), pd.to_datetime(p.horario_prevista_terminal).strftime('%H:%M')):p for p in Partida.objects.select_related().all()}
     print(list(dic_partidas.keys())[0])
-    '''
     cartoes = np.vectorize(lambda numero: Cartao(
         numero=numero,
         tipo_id=1,
     ))(
-        df1.CARTAO.values,
+        df1.CARTAO.unique(),
     )
     Cartao.objects.bulk_create(list(cartoes))
-    '''
     dic_cartaos = {p.numero:p for p in Cartao.objects.select_related().all()}
     #print(dic_cartaos)
     dic_pontos = {p.codigo_cittamobi:p for p in PontoPorEmpresa.objects.select_related().all()}
     print(dic_pontos)
     viagens = np.vectorize(lambda linha, carro, horario, partida, cartao, ponto_embarque: ViagemDePassageiro(
-        partida_embarque=dic_partidas[(str(int(linha)), str(carro), pd.to_datetime(partida).date(), pd.to_datetime(partida).time())],
-        horario=pd.to_datetime(horario),
-        ponto_embarque=dic_pontos[ponto_embarque],
+        partida_embarque=dic_partidas.get((int(linha), str(carro), pd.to_datetime(partida).strftime('%d/%m/%Y'), pd.to_datetime(partida).strftime('%H:%M'))),
+        horario=maceio_tz.localize(pd.to_datetime(horario)),
+        ponto_embarque=dic_pontos.get(ponto_embarque),
         cartao=dic_cartaos[cartao],
     ))(
         df1.LINHA.values,
@@ -133,10 +138,30 @@ def create_viagens(df):
 
 
 #MAIN
-#df_tv = pd.read_pickle(pre + 'departures total'+ pos +'.zip', compression='zip')
-#create_lines(df_tv)
-#df_pontos = pd.read_excel(pre + '/pontos'+ pos +'.xlsx')
-#create_pontos(df_pontos)
-df_pass = pd.read_pickle(pre + 'PassageirosPorPonto/passageiros_pontos 0716'+ pos +'.zip', compression='zip')
+df_tv = pd.read_pickle(pre + 'departures total'+ pos +'.zip', compression='zip')
+create_lines(df_tv)
+df_pontos = pd.read_excel('../ScriptsIniciais/base_pontos'+ pos +'.xlsx')
+print(df_pontos)
+create_pontos(df_pontos)
+print('Pontos Criados')
+arquivos = [pre + 'PassageirosPorPonto/'+f for f in os.listdir(pre + 'PassageirosPorPonto/') if f.endswith('.zip')]
+dfs = []
+print(arquivos)
+for a in arquivos:
+    df = pd.read_pickle(a, compression='zip')
+    dfs.append(df)
+df_pass = pd.concat(dfs, ignore_index=True)
+dfs = None
+df = None
+
+print(len(df_pass.partida.unique()))
+print(df_pass.count())
+df_pass.linha
+
+
+ 
 create_viagens(df_pass)
-errrrr
+print('Dados de passageiros finalizados')
+print('Criando ordem dos pontos')
+from read_stop_orders import *
+print('\n\n\n\n------------------\n\nFinalizado!\n')
